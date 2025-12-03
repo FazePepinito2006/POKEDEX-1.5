dde4bb0f7cc4e73c4b0510c60b490934b356641a
FazePepinito2006 <francisco.tapiam2006@gmail.com>
2025-12-03 01:21:35 -0300

    Update solemne_3 (3).py

--- solemne_3 (3).py
+++ solemne_3 (3).py
@@ -185,11 +185,3 @@ if tipo_seleccionado:
                 #Interpretación (Actualizada para coincidir con el gráfico)
                 st.info(f"**Análisis:** Se observa la distribución detallada de stats para el tipo {tipo_seleccionado}. "
                         f"Este gráfico permite comparar directamente qué Pokémon es superior en cada atributo específico.")
-
-                    # --- GRÁFICO 2: DISPERSIÓN (SCATTER) ---
-                    st.markdown("---") # Línea separadora
-                    st.subheader("2. Relación Altura vs Peso")
-                    st.caption("Gráfico de dispersión para analizar la contextura física.")
-                
-                    # Este gráfico usa las columnas 'Peso' y 'Altura'
-                    st.scatter_chart(df_comparativo, x='Peso', y='Altura')
